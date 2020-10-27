2eafc1b00fd8f456b99d08237759bd593c9882e9
David Lonni <david_lonni@hotmail.fr>
2020-10-27 23:43:43 +0100

    Intro
    - Created Intro scene

---
+++ Lost in the sea/Assets/Scripts/KeepOffset.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class KeepOffset : MonoBehaviour
+{
+    public Transform m_Target;
+
+    void Update()
+    {
+        transform.position = new Vector3(m_Target.position.x, transform.position.y, m_Target.position.z);
+    }
+}
