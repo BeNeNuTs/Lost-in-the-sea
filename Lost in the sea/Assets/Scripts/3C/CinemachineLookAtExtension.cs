efedb3ff8bc696de80dc64d1cf1abfc7ff91b3cb
David Lonni <david_lonni@hotmail.fr>
2020-11-05 21:52:11 +0100

    3C
    - Inputs settings & actions
    - Added MainCharacterController to move the character and update his speed
    - Added CinemachineLookAtExtention to handle new input system
    - Updated MainCharacter.prefab
    - Created MC animator controller

---
+++ Lost in the sea/Assets/Scripts/3C/CinemachineLookAtExtension.cs
@@ -0,0 +1,19 @@
+﻿using Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CinemachineFreeLook))]
+public class CinemachineLookAtExtension : CinemachineExtension
+{
+    public CinemachineFreeLook m_CinemachineFreeLook;
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        Vector2 lookMovement = context.ReadValue<Vector2>().normalized;
+        m_CinemachineFreeLook.m_XAxis.m_InputAxisValue = lookMovement.x;
+        m_CinemachineFreeLook.m_YAxis.m_InputAxisValue = lookMovement.y;
+    }
+
+    protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    {}
+}
---
+++ Lost in the sea/Assets/Scripts/3C/MainCharacterController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class MainCharacterController : MonoBehaviour
+{
+    public CharacterController m_CharacterController;
+    public Transform m_Camera;
+    public Animator m_Animator;
+    public float m_Speed = 3f;
+
+    private Vector3 m_LastInputMoveDirection;
+    private Vector3 m_CurrentVelocity;
+
+    private void Update()
+    {
+        Move();
+        LookAt();
+
+        UpdateSpeed();
+    }
+
+    private void Move()
+    {
+        Vector3 desiredMoveDirection = GetDesiredMoveDirection();
+        m_CurrentVelocity.x = desiredMoveDirection.x;
+        m_CurrentVelocity.z = desiredMoveDirection.z;
+
+        m_CharacterController.Move(m_CurrentVelocity * Time.deltaTime * m_Speed);
+
+        m_CurrentVelocity.y += Physics.gravity.y;
+        if (m_CharacterController.isGrounded && m_CurrentVelocity.y < 0f)
+            m_CurrentVelocity.y = 0f;
+    }
+
+    private void LookAt()
+    {
+        if (m_CurrentVelocity.x != 0f || m_CurrentVelocity.z != 0f)
+            transform.forward = new Vector3(m_CurrentVelocity.x, 0f, m_CurrentVelocity.z);
+    }
+
+    private void UpdateSpeed()
+    {
+        Vector3 groundVelocity = new Vector3(m_CharacterController.velocity.x, 0f, m_CharacterController.velocity.z);
+        m_Animator.SetFloat("Speed", groundVelocity.magnitude);
+    }
+
+    private Vector3 GetDesiredMoveDirection()
+    {
+        Vector3 forward = m_Camera.forward;
+        Vector3 right = m_Camera.right;
+
+        //project forward and right vectors on the horizontal plane (y = 0)
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+
+        //this is the direction in the world space we want to move:
+        return forward * m_LastInputMoveDirection.z + right * m_LastInputMoveDirection.x;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 inputMove = context.ReadValue<Vector2>();
+        m_LastInputMoveDirection = new Vector3(inputMove.x, 0f, inputMove.y);
+    }
+}
